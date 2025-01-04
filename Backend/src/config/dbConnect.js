2d84c1bc354525b80185dbababf6dd74d3534b06
Sunnoogo77 <sunnoogo-caleb.tapsoba@efrei.net>
2025-01-04 23:15:40 +0000

    push 01

--- Backend/src/config/dbConnect.js
+++ Backend/src/config/dbConnect.js
@@ -5,14 +5,7 @@ dotenv.config();
 export const dbConnect = async () => {
     try {
         const connectionString = "mongodb+srv://tapsobascaleb:zsQmR6uysgHFFFTB@2fa-cluster.fqmrt.mongodb.net/?retryWrites=true&w=majority&appName=2fa-cluster";
-        
-        if (!connectionString) {
-            throw new Error('CONNECTION_STRING environment variable is not defined');
-        }
         const mongoDbConnection = await connect(connectionString);
-        if (!mongoDbConnection) {
-            throw new Error('CONNECTION_STRING environment variable is not defined');
-        }
         console.log(`MongoDB connection SUCCESS : ${mongoDbConnection.connection.host}`);
     } catch (error) {
         console.log(`Database connection failed: ${error}`);
