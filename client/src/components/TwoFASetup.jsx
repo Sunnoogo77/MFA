39ed4eceedc37c07ca86aa5713453415f832118b
Sunnoogo77 <sunnoogo-caleb.tapsoba@efrei.net>
2025-01-08 19:27:56 +0000

    08-01

--- client/src/components/TwoFASetup.jsx
+++ client/src/components/TwoFASetup.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { setup2FA } from '../service/authApi.js';
 
-const TwoFASetup = () => {
+
+const TwoFASetup = ({onSetupComplete}) => {
+    const [response, setResponse] = useState({});
+    const [message, setMessage] = useState('');
+
+    const fetchQrCode = async() => {
+        const {data} = await setup2FA();
+        //console.log(data);
+        setResponse(data);
+    };
+
+    useEffect(() => {
+        fetchQrCode();
+    }, []);
+
+    const copyClipBoard = async() => {
+        try {
+            await navigator.clipboard.writeText(response.secret);
+            setMessage('Copied to clipboard');
+            alert('Copied to clipboard');
+        } catch (error) {
+            alert('Failed to copy to clipboard');
+        }
+    };
     return (
         <div 
             className  ='bg-white rounded-lg shadow-md w-full max-w-sm mx-auto'
@@ -14,32 +38,36 @@ const TwoFASetup = () => {
             <p className='text-center text-gray-600 text-lg font-light pr-6 pl-6'> 
                 Scan the QR code below using your 2FA app to enable 2FA
             </p>
-            <div className='flex justify-center'>
-                <img 
-                    src='https://via.placeholder.com/150'
-                    alt='2FA QR Code'
-                    className='mb-4 border rounded-mb mt-6'
-                />
+            <div className='p-6'>
+                <div className='flex justify-center'>
+                    <img 
+
+                        src={response.QRCode}
+                        alt='2FA QR Code'
+                        className='mb-4 border rounded-mb mt-6'
+                    />
+                </div>
+                <div className='flex items-center mt-3 mb-3'>
+                    <div className='border-t border-1 border-gray-200 flex-grow'></div>
+                    <div className='text-gray-600 text-sm font-light pr-2 pl-2'>Or enter the Code Manually</div>
+                    <div className='border-t border-1 border-gray-200 flex-grow'></div>
+                </div>
+                <div className='mb-6'>
+                    {message && <p className='text-center text-sm text-green-500 mb-3'>{message}</p>}
+                    <input readOnly 
+                        defaultValue="" 
+                        value={response.secret}
+                        className='w-full border rounded mt-2 text-xs text-gray-600 p-4'
+                        onClick={copyClipBoard}
+                    />
+                </div>
+                <button 
+                    onClick={onSetupComplete} 
+                    className='w-ful bg-blue-500 text-white py-2 rounded-md'
+                >
+                    Continue to Verification
+                </button>
             </div>
-            <div className='flex items-center mt-3 mb-3'>
-                <div className='border-t border-1 border-gray-200 flex-grow'></div>
-                <div className='text-gray-600 text-sm font-light pr-2 pl-2'>Or enter the Code Manually</div>
-                <div className='border-t border-1 border-gray-200 flex-grow'></div>
-            </div>
-            <div className='mb-6'>
-                <input readOnly 
-                    defaultValue="" 
-                    value="" 
-                    className='w-full border rounded mt-2 text-xs text-gray-600 p-4'
-                    onClick={copyClipBoard}
-                />
-            </div>
-            <button 
-                onClick={onSetupComplete} 
-                className='w-ful bg-blue-500 text-white py-2 rounded-md'
-            >
-                Continue to Verification
-            </button>
         </div>
     );
 };
--- client/src/context/SessionContext.jsx
+++ client/src/context/SessionContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { data } from 'react-router-dom';
 
 const SessionContext = createContext();
@@ -9,15 +9,25 @@ export const SessionProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState(null);
 
+    useEffect(() => {
+        const storeUser = JSON.parse(sessionStorage.getItem('user'));
+        console.log("The useEffect runs ; ", storeUser);
+        if (storeUser) {
+            setIsLoggedIn(true);
+            setUser(storeUser);
+        }
+
     const login = (userData) => {
         setIsLoggedIn(true);
         setUser(userData);
+        sessionStorage.setItem('user', JSON.stringify(userData));
     };
 
     const logout = (data) => {
         if (data) {
             setIsLoggedIn(false);
             setUser(null);
+            sessionStorage.removeItem('user');
         }
     };
 
